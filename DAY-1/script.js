de7a7a450511e1f743084ec5139e167b0bdcdffb
Nabarajrai <nabaraj2055@gmail.com>
2025-10-29 22:55:59 +0545

    find indices of two value of target

---
+++ DAY-1/script.js
@@ -0,0 +1,15 @@
+//two sum with target
+const twoSumIndexes = (nums, target) => {
+  const numMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (numMap.has(complement)) {
+      return [numMap.get(complement), i];
+    }
+    numMap.set(nums[i], i);
+  }
+  return [];
+};
+
+console.log(twoSumIndexes([2, 7, 11, 15], 9)); // Output: [0, 1]
+console.log(twoSumIndexes([3, 2, 4], 6)); // Output: [1, 2]
