517c096648aa79179adec33ea5ec5ee215e7faad
Nabarajrai <nabaraj2055@gmail.com>
2025-11-03 09:21:20 +0545

    update

---
+++ DAY-2/script.js
@@ -0,0 +1,6 @@
+let students = [
+  { name: "Alice", score: 92, subjects: ["Math", "Science"] },
+  { name: "Bob", score: 78, subjects: ["Math", "History"] },
+  { name: "Charlie", score: 95, subjects: ["Science", "Art"] },
+  { name: "Dana", score: 65, subjects: ["History", "Art"] },
+];
